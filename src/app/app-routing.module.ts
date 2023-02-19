1e1e05587e9aea22703cfaa04c41c0c2c90dc7b3
Ashink12 <ashin2001boom@gmail.com>
2023-02-20 00:42:48 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { MeanComponent } from './mean/mean.component'; 
 import { LoginComponent } from './login/login.component';
-import { AdminDashboardComponent } from './modules/admin/components/admin-dashboard/admin-dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [{path:'login', component:LoginComponent},
-{path:'signup', component:SignupComponent},
+{path:'signup', component:SignupComponent },
 {path:'mean',component: MeanComponent},
 {path:'forgot-password',component:ForgotPasswordComponent},
-{path:'not-found', component:NotFoundComponent},{path:'',redirectTo:'/login',pathMatch:'full'},
-{path:'admin', 
- loadChildren: () => import('./modules/admin/admin.module').then((m) =>m.AdminModule)}]
-;
+{path:'not-found', component:NotFoundComponent},{path:'',redirectTo:'/login',pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
